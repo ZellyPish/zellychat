464eb0a4c6eb04e9ab3e198e927eea2778ab7227
ZellyPish <ehxhfl21@gmail.com>
2021-01-29 02:03:21 +0900

    Add: Login and Register Pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,10 @@ import { Switch, Route } from 'react-router-dom'
 import { jsx, css, Global } from '@emotion/react'
 import emotionReset from 'emotion-reset'
 
-import './styles/tailwind.css'
-
 import Login from './components/Auth/Login/Login'
 import Register from './components/Auth/Register/Register'
 import Home from './components/Home'
+import tw from 'twin.macro'
 
 const App = () => (
   <React.Fragment>
@@ -21,6 +20,7 @@ const App = () => (
           -webkit-font-smoothing: antialiased;
           font-smoothing: antialiased;
         }
+        body {${tw`font-sans bg-purple-200`}}
       `}
     />
 
